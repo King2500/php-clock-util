1252543f9a7afeccd2f3ba06c4465a35b3e09350
Thomas Schulz <mail@king2500.net>
2015-05-21 19:56:53 +0200

    Added basic file and folder structure

---
+++ src/Clock.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace ThomasSchulz\ClockUtil;
+
+class Clock
+{
+
+}
---
+++ tests/ClockTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace ThomasSchulz\ClockUtil\Tests;
+
+class ClockTest extends \PHPUnit_Framework_TestCase
+{
+
+}
