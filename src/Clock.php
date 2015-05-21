19e54a71740f232c64f3bae950db7e3c86ddb21d
Thomas Schulz <mail@king2500.net>
2015-05-21 20:06:47 +0200

    First basic implementation of 'now' method

--- src/Clock.php
+++ src/Clock.php
@@ -4,5 +4,18 @@ namespace ThomasSchulz\ClockUtil;
 
 class Clock
 {
+    private static $currentTime = null;
 
+    public static function now($now = null)
+    {
+        if ($now !== null) {
+            static::$currentTime = $now;
+        }
+
+        if (static::$currentTime !== null) {
+            return static::$currentTime;
+        }
+
+        return new \DateTime();
+    }
 }
