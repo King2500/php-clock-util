69cccf9a265b304df70cad9ca107393b14816f49
Thomas Schulz <mail@king2500.net>
2016-05-16 15:55:01 +0200

    Added PhpDoc comments

--- src/Clock.php
+++ src/Clock.php
@@ -2,10 +2,24 @@
 
 namespace ThomasSchulz\ClockUtil;
 
+/**
+ * Clock util class.
+ * 
+ * @author Thomas Schulz <mail@king2500.net>
+ */
 class Clock
 {
+    /**
+     * @var \DateTime|null
+     */
     private static $currentTime = null;
 
+    /**
+     * Get or set current time.
+     * 
+     * @param \DateTime|null $now
+     * @return \DateTime
+     */
     public static function now($now = null)
     {
         if ($now !== null) {
